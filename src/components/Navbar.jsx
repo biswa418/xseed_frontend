b99507b233bd0a63ec7e522eef0431ff61758cf3
Biswajeet Sutar <biswajeetsutar418@gmail.com>
2023-06-30 00:03:51 +0530

    navbar fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,25 @@
 import React, { useState } from 'react'
 import { IoMenuOutline } from 'react-icons/io5'
 import { GrClose } from 'react-icons/gr';
+import { subjects } from '../utils/constants'
 
 
 const Navbar = () => {
     const [openMenu, setOpenmenu] = useState(false);
+    const [openSubMenu, setSubOpenmenu] = useState(false);
 
     function handleChangeMenu() {
         setOpenmenu(!openMenu);
+    }
+
+    function handleSubChangeMenu(e) {
+        setSubOpenmenu(!openSubMenu);
+        e.classList.toggle('animateHeight');
+    }
+
+    function showContent(listtoShow, parentElement) {
+        listtoShow.classList.toggle('active');
+        parentElement.classList.toggle('animateHeight');
     }
 
     return (
@@ -18,32 +30,92 @@ const Navbar = () => {
             </div>
 
 
-            <div className='md:block hidden'>
+            <div className='md:flex items-center hidden'>
+                <>
+                    <div onClick={(e) => handleSubChangeMenu(e.target)} className='rounded-full cursor-pointer active px-4 py-2'>
+                        Subjects
+                    </div>
+
+                    {
+                        openSubMenu &&
+                        <div className='side_menu relative'>
+                            <div className='flex flex-col items-center bg-slate-300 backdrop-blur-lg rounded-lg text-base absolute -right-40 top-10 z-50 w-[92vw] min-h-[50vh] pb-8 h-max'>
+                                <ul className='w-3/4 mt-4'>
+                                    {
+                                        subjects.map((sub) => {
+                                            return (
+                                                <li key={sub.id} value={sub.id} className='my-2 flex flex-col cursor-pointer w-full rounded-md bg-slate-50'>
+                                                    <div className='w-full py-2 px-4 hover:text-white hover:bg-slate-400' onClick={(e) => showContent(e.target.nextSibling, e.target)}>
+                                                        {sub.name}
+                                                    </div>
+
+                                                    <ul className='text-left w-3/4 content'>
+                                                        {
+                                                            sub.sub.map((eachSub, id) => {
+                                                                return (
+                                                                    <li key={id} className='my-1 text-slate-400 hover:text-slate-600 text-sm'>
+                                                                        {eachSub}
+                                                                    </li>
+                                                                )
+                                                            })
+                                                        }
+                                                    </ul>
+                                                </li>
+                                            )
+                                        })
+                                    }
+                                </ul>
+                            </div>
+                        </div>
+                    }
+                </>
                 <a href='/login' className='mx-3 text-blue-600'>Sign in</a>
                 <a href='/register' className='mx-3 px-3 py-2 bg-blue-600 text-white rounded-full'>Sign up</a>
             </div>
 
             <div className='md:hidden flex items-center'>
-                <button className='text-3xl text-gray-600' onClick={handleChangeMenu}>
+                <button className='text-3xl text-gray-600'>
                     {
                         !openMenu &&
-                        <IoMenuOutline />
+                        <IoMenuOutline onClick={handleChangeMenu} />
                     }
 
                     {
                         openMenu &&
                         <>
-                            <GrClose />
+                            <GrClose onClick={handleChangeMenu} />
                             <div className='side_menu relative'>
-                                <div className='flex flex-col items-center bg-slate-300 backdrop-blur-lg rounded-lg text-base absolute right-0 top-5 z-50 w-[92vw] h-[50vh]'>
+                                <div className='flex flex-col items-center bg-slate-300 backdrop-blur-lg rounded-lg text-base absolute right-0 top-5 z-50 w-[92vw] min-h-[50vh] pb-8 h-max'>
                                     <div className='w-3/4 bg-slate-500 text-white rounded-full flex justify-around p-2 mt-4'>
                                         <a href='/login' className='mx-1 whitespace-nowrap'>Sign in</a>
                                         |
                                         <a href='/register' className='mx-1 whitespace-nowrap'>Sign up</a>
                                     </div>
 
-                                    <ul>
-                                        {/* to be added later */}
+                                    <ul className='w-3/4 mt-4'>
+                                        {
+                                            subjects.map((sub) => {
+                                                return (
+                                                    <li key={sub.id} value={sub.id} className='my-2 flex flex-col items-center  w-full rounded-md bg-slate-50'>
+                                                        <div className='w-full py-2 hover:text-white hover:bg-slate-400' onClick={(e) => showContent(e.target.nextSibling, e.target)}>
+                                                            {sub.name}
+                                                        </div>
+
+                                                        <ul className='text-left w-3/4 p-2 content'>
+                                                            {
+                                                                sub.sub.map((eachSub, id) => {
+                                                                    return (
+                                                                        <li key={id} className='my-1 text-slate-400 text-sm'>
+                                                                            {eachSub}
+                                                                        </li>
+                                                                    )
+                                                                })
+                                                            }
+                                                        </ul>
+                                                    </li>
+                                                )
+                                            })
+                                        }
                                     </ul>
                                 </div>
                             </div>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -8,6 +8,26 @@ export const subjects = [
     {
         'id': 2,
         'name': 'Conventional Math',
-        'sub': ['']
+        'sub': ['Elementary Math (Grade 1-5)', 'Math 6', 'Math 7', 'Math 8', 'Algebra I', 'Geometry', 'Algebra II', 'Probability & Statistics']
+    },
+    {
+        'id': 3,
+        'name': 'Science',
+        'sub': ['Grade K to 5', 'Earth Science', 'Life Science', 'Physical Science', 'Biology']
+    },
+    {
+        'id': 4,
+        'name': 'English',
+        'sub': ['Writing', 'Spelling']
+    },
+    {
+        'id': 5,
+        'name': 'Social Studies',
+        'sub': ['Economics', 'Geography', 'Government', 'History', 'World History', 'Philosophy']
+    },
+    {
+        'id': 6,
+        'name': 'More',
+        'sub': ['Astronomy', 'Engineering', 'Health', 'Technology']
     }
 ]
